e797a6a5b47a1c123a9647186c697e7d2da8aaf4
jimnetro <128082475+jimnetro@users.noreply.github.com>
2023-12-14 18:57:19 +0300

    Add project files.

---
+++ PDF_Reader/Models/SaleDetail.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace PDF_Reader.Models
+{
+    public class SaleDetail
+    {
+        [Key]
+        public int ID { get; set; }
+        [ForeignKey(nameof(Sale))]
+        public int Sale_ID { get; set; }
+
+        [JsonIgnore]
+        public virtual Sale? Sale { get; set; }
+
+        [ForeignKey("ProductVariant")]
+        public int ProductVariant_ID { get; set; }
+        //public virtual ProductVariant? ProductVariant { get; set; }
+        public int? ProductVariantPriceBreak_ID { get; set; }
+        [ForeignKey(nameof(ProductVariantPriceBreak_ID))]
+        //public ProductVariantPriceBreak? ProductVariantPriceBreak { get; set; }
+        public int? CustomerPriceBandID { get; set; }
+        [ForeignKey(nameof(CustomerPriceBandID))]
+        //public CustomerPriceBand? CustomerPriceBand { get; set; }
+        public int? Company_ID { get; set; }
+        [ForeignKey(nameof(Company_ID))]
+        //public Company? Company { get; set; }
+        public short Qty { get; set; }
+        public decimal ShouldSalesPrice { get; set; }
+        public decimal SalesPrice { get; set; }
+        public decimal CostPrice { get; set; }
+        public decimal ACP { get; set; }
+        public string Note { get; set; }
+        public string? Description { get; set; }
+        public decimal? VAT { get; set; }
+        public decimal? LineTotalVAT { get; set; }
+        public decimal? VATRate { get; set; }
+        [StringLength(20)]
+        public string VATCode { get; set; }
+        public decimal? Discount { get; set; }
+        public decimal? ItemDiscount { get; set; }
+        public decimal? BasketDiscount { get; set; }
+        [NotMapped]
+        public bool ZeroStocked { get; set; }
+
+        public short? PriceBreakQty { get; set; }
+        public decimal? PriceBreakSalePrice { get; set; }
+        //public List<SaleDetailSerialNo>? SerialNos { get; set; }
+        public int? CommissionStaff_ID { get; set; }
+        public decimal? Commission { get; set; }
+        public decimal? CommissionRate { get; set; }
+        public string? VoucherData { get; set; }
+        [NotMapped]
+        public bool PreventStockIn { get; set; }
+    }
+}
---
+++ PDF_Reader/Pages/Index.cshtml.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+using System.Reflection.PortableExecutable;
+using Microsoft.AspNetCore.Hosting.Server;
+using System;
+using System.IO;
+
+
+namespace PDF_Reader.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPostPdfText(IFormFile fileUpload)
+        {
+            if (fileUpload != null && fileUpload.Length > 0)
+            {
+                // Get the file name
+                var fileName = System.IO.Path.GetFileName(fileUpload.FileName);
+
+                // Get the path
+                var path = System.IO.Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+
+                // Now you have the path, you can process it as needed
+                //string path = "file:///M:/taha_programing_file/C%23/PDF%20Reader/Test%20Files/11428111130.pdf";
+                PdfReader reader = new PdfReader(path);
+            string text = string.Empty;
+
+            for (int page = 1; page <= reader.NumberOfPages; page++)
+            {
+                text += PdfTextExtractor.GetTextFromPage(reader, page);
+            }
+
+            Console.WriteLine(text);
+
+            reader.Close();
+            }
+            return Page();
+        }
+    }
+}
